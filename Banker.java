193e82aef1a06fb5fce2f602748f7d598646aa86
Aidan Ainsworth <ainswoa2@wwu.edu>
2023-04-29 16:23:01 -0700

    Banker skeleton

--- Banker.java
+++ Banker.java
@@ -1,3 +1,11 @@
 public class Banker {
-    
+    private static void payPlayer(Player player, int amount, String currency) {
+
+    }
+    private static boolean verifyFunds(Player player, String currency){
+        return false;
+    }
+    private static boolean removeFunds(Player player, int amount, String currency) {
+        return false;
+    }
 }
