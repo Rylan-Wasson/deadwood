88626be4c8ffa9406d69238443de4a4091433991
Aidan Ainsworth <ainswoa2@wwu.edu>
2023-04-29 16:44:59 -0700

    Banker skele updated, Scene Skele made

--- Banker.java
+++ Banker.java
@@ -1,11 +1,18 @@
+import java.util.LinkedList;
+
 public class Banker {
     private static void payPlayer(Player player, int amount, String currency) {
-
     }
+
     private static boolean verifyFunds(Player player, String currency){
         return false;
     }
+
     private static boolean removeFunds(Player player, int amount, String currency) {
         return false;
+    }
+
+    private static void sceneBonus(LinkedList<Player> actors, int budget) {
+
     }
 }
--- Scene.java
+++ Scene.java
@@ -1,3 +1,28 @@
 public class Scene {
-    
+
+    private String name;
+    private int shot_count;
+    private int budget;
+
+    public Scene(String name, int shot_count, int budget){
+        this.name = name;
+        this.shot_count = shot_count;
+        this.budget = budget;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getShotCount(){
+        return this.shot_count;
+    }
+
+    public int getBudget(){
+        return this.budget;
+    }
+
+    public void setShotCount(int newCount){
+        this.shot_count = newCount;
+    }
 }
