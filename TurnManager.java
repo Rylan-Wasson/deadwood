12a3e112860c515bc1eb62ac34098e3f6f521726
Rylan Wasson <wassonr@wwu.edu>
2023-04-29 17:47:03 -0700

    TurnManager skeleton code

--- TurnManager.java
+++ TurnManager.java
@@ -1,3 +1,33 @@
 public class TurnManager {
-    
+    private Boolean turnActive;
+    private Boolean hasMoved;
+    private Player activePlayer;
+
+    public void conductTurn(Player player){
+
+    }
+
+    private void moveAction(){
+
+    }
+
+    private void takeRoleAction(){
+        
+    }
+
+    private void actRoleAction(){
+
+    }
+
+    private void rehearseAction(){
+
+    }
+
+    private void upgradeAction(){
+
+    }
+
+    private void endTurnAction(){
+
+    }
 }
