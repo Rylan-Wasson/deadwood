fd94b43b8a5435d02f127690e54220e843defd07
Aidan Ainsworth <ainswoa2@wwu.edu>
2023-04-29 17:54:05 -0700

    GameManager getters/setters done

--- GameManager.java
+++ GameManager.java
@@ -1,12 +1,34 @@
 import java.util.LinkedList;
 
 public class GameManager {
-    private int numDays;
-    private int numPlayers;
+    private int num_days;
+    private int num_players;
     private LinkedList<Player> players;
 
-    
-    private int playDay(){
-        return -1;
+    //Getters
+    public int getNumDays(){
+        return this.num_days;
+    }
+
+    public int getNumPlayers(){
+        return this.num_players;
+    }
+
+    public LinkedList<Player> getPlayers(){
+        return this.players;
+    }
+
+    //Setters
+    public void setNumDays(int num_days){
+        this.num_days = num_days;
+    }
+
+    public void setNumPlayers(int num_players){
+        this.num_players = num_players;
+    }
+
+    //Util functions
+    private void playDay(){
+
     }
 }
