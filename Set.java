1654a12d20f5654e60c9b2eac3c8a59344d5f5c2
Rylan Wasson <rtw0329@gmail.com>
2023-04-30 17:35:07 -0700

    Set skeleton code

--- Set.java
+++ Set.java
@@ -1,3 +1,41 @@
+import java.util.ArrayList;
+
 public class Set {
-    
+    private String name;
+    private ArrayList<Set> nearby_sets;
+    private int shot_counters;
+    private Scene set_scene;
+
+    public Set(String name, ArrayList<Set> nearby_sets, int shot_counters, Scene set_scene){
+
+    }
+
+
+    // -----Accessors-----
+
+    public String getName(){
+        return name;
+    }
+
+    public ArrayList<Set> getNearbySets(){
+        return nearby_sets;
+    }
+
+    public int getShotCounters(){
+        return shot_counters;
+    }
+
+    public Scene getScene(){
+        return set_scene;
+    }
+
+    // -----Mutators-----
+
+    public void setShotCounters(int counters){
+        this.shot_counters = counters;
+    }
+
+    public void setScene(Scene scene){
+        this.set_scene = scene;
+    }
 }
--- TurnManager.java
+++ TurnManager.java
@@ -2,7 +2,12 @@ public class TurnManager {
     private Boolean turn_active;
     private Boolean has_moved;
     private Player active_player;
+    private TextController controller;
 
+    public TurnManager(TextController controller){
+
+    }
+    
     public void conductTurn(Player player){
 
     }
