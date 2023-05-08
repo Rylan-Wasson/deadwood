5186cb90356fe56838905063777c247842f9b559
Rylan Wasson <rtw0329@gmail.com>
2023-05-08 16:34:03 -0700

    Banker implementation

--- Banker.java
+++ Banker.java
@@ -1,15 +1,42 @@
 import java.util.LinkedList;
 
 public class Banker {
-    private void payPlayer(Player player, int amount, String currency) {
+    public void payPlayer(Player player, int amount, String currency) {
+        if(currency.equals("credits")){
+            player.setCredits(player.getCredits() + amount);
+        } else if(currency.equals("cash")){
+            player.setCash(player.getCash() + amount);
+        } else if(currency.equals("rehearse_chips")){
+            player.setRehearse_chips(player.getRehearse_chips() + amount);
+        }
     }
 
-    private boolean verifyFunds(Player player, String currency){
+    private boolean verifyFunds(Player player, int amount, String currency){
+        if(currency.equals("credits")){
+            if(player.getCredits() > amount){
+                return true;
+            }
+        } else if(currency.equals("cash")){
+            if(player.getCash() > amount){
+                return true;
+            }
+        } else if(currency.equals("rehearse_chips")){
+            if(player.getRehearse_chips() > amount){
+                return true;
+            }
+        }
+        
         return false;
     }
 
-    private boolean removeFunds(Player player, int amount, String currency) {
-        return false;
+    public void removeFunds(Player player, int amount, String currency) {
+        if(currency.equals("credits")){
+            player.setCredits(player.getCredits() - amount);
+        } else if(currency.equals("cash")){
+            player.setCash(player.getCash() - amount);
+        } else if(currency.equals("rehearse_chips")){
+            player.setRehearse_chips(player.getRehearse_chips() - amount);
+        }
     }
 
     private void sceneBonus(LinkedList<Player> actors, int budget) {
