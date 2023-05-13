39523d3c3ff254b2dec61df41ce40c33485ab471
Rylan Wasson <wassonr@cf164-24.cs.wwu.edu>
2023-05-13 13:20:30 -0700

    finished banker implementation

--- Banker.java
+++ Banker.java
@@ -1,4 +1,8 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class Banker {
     /* adds amount of type currency to player */
@@ -51,8 +55,45 @@ public class Banker {
     }
 
     
+    /* Distribute budget d6 rolls worth of cash for the scene bonus,
+     * assumes actors is sorted by role level in ascending order
+     * also assumes actors is not null
+     */
+    public void sceneBonus(ArrayList<Player> actors, int budget) {
+        int[] rolls = rollXDice(budget);
+        int j = actors.size()-1; // actors index
+        Arrays.sort(rolls);
 
-    private void sceneBonus(LinkedList<Player> actors, int budget) {
+        /* Iterate through dice rolls in descending order */
+        for(int i = rolls.length-1; i >= 0; i--){
+            if(j < 0){
+                j = actors.size()-1;
+            }
+            payPlayer(actors.get(j), rolls[i], "cash");
+            j--;
+        }
+    }
+    /*  Distribute a cash bonus to each actors equal to
+     *  the level of their currently worked role
+     */
+    public void extraBonus(ArrayList<Player> actors){
+        for(int i = 0; i < actors.size(); i++){
+            Player actor = actors.get(i);
+            int bonus = actor.getPlayerRole().getRank();
+            payPlayer(actor, bonus, "cash");
+        }
+    }
 
+    /*
+     * Rolls X dice and returns an int[] of the generate rolls 
+     */
+    public int[] rollXDice(int num_dice){
+        Random randomInt = new Random();
+        int[] dice_outcomes = new int[num_dice];
+        
+        for(int i = 0; i < num_dice; i++){
+            dice_outcomes[i] = (randomInt.nextInt(6) + 1); //exclusive upper bound
+        }
+        return dice_outcomes;
     }
 }
